c17366ff87a1abed7a2e4d1081222d8548f18dc0
Phil Bri <ph.brioude@gmail.com>
2014-12-01 14:08:55 +0100

    V2.0
    Automatic IP searching

--- LiveboxRemote.js
+++ LiveboxRemote.js
@@ -1,55 +1,55 @@
 /*__________________________________________________
-| LiveBoxRemote V 0.9								|
+| LiveBoxRemote V 2.0								|
 | Plugin pour S.A.R.A.H. 							|
 | ( By Phil Bri 04/2014 )							|
 |___________________________________________________|
 */
 
+var LiveBoxIP;
+
+exports.init = function ( SARAH ) {
+
+	var findAdrs = require ( './lib/findUPNP.js' );
+
+	findAdrs( 'livebox', 'UHD', function ( BoxIP ) {
+		if ( !BoxIP ) { return console.log ( '\r\nLiveBoxRemote => LiveBox non trouvée\r\n' ) }
+		LiveBoxIP = BoxIP;
+		console.log ( '\r\nLiveBoxRemote => Livebox IP = ' + LiveBoxIP + '\r\n');
+	});
+}
 
 exports.action = function ( data , callback , config , SARAH ) {
-
+	
+	if ( LiveBoxIP == undefined ) { return callback ({ 'tts' : 'Live box non trouvée' }) }
 
 	config = config.modules.liveboxremote;
 	var cmdArray = data.LBCode.split(',');
 
-
 	sendLiveBox ( cmdArray );
 
-
 	function sendLiveBox ( cmdArray ) {
-		// Test IP
-		if ( !config.LiveBoxIP ) {
-			console.log ( 'Missing LiveBox IP in liveboxremote.prop !' );
-			callback 	({ 'tts' : 'Adresse I P absente' });
-			return;
-		}
-
 
 		// Configure the request : NOTE -> The {qs} option add { ?operation=01&key=[LBCmd[123]]&mode=[LBCmd[5]] } at the end of {url}
 		var LBCmd 	= cmdArray.shift();
 		var request = require ( 'request' );
-		var options = 	{	url     : 	'http://' + config.LiveBoxIP + ':8080/remoteControl/cmd',
-    						qs      : { 'operation'	: '01', 'key' : LBCmd.substr(0,3) , 'mode' : LBCmd.substr(4,1) }
+		var options = 	{	url	: 	'http://' + LiveBoxIP + ':8080/remoteControl/cmd',
+    						qs	: { 'operation'	: '01', 'key' : LBCmd.substr(0,3) , 'mode' : LBCmd.substr(4,1) }
 						}
 
-
-		// Start the request
 		request( options , function ( error , response , body ) {
-    			if ( !error && response.statusCode == 200 ) {
 
+			if ( !error && response.statusCode == 200 ) {
 
-    				if ( cmdArray.length ) {
-    					sendLiveBox(cmdArray);
-    				}
-
-
-    				console.log ( 'LiveBox Cmd : ' + data.LBCode + ' = OK' );
-    				callback ({ 'tts': data.ttsAction });
-
-
-    			} else {
-    				callback ({ 'tts': 'erreur commande box' });
+				if ( cmdArray.length ) {
+					sendLiveBox(cmdArray);
     			}
+
+    			console.log ( '\r\nLiveBoxRemote => Cmd : ' + data.LBCode + ' = OK\r\n' );
+    			callback ({ 'tts': data.ttsAction });
+
+    		} else {
+    			callback ({ 'tts': 'erreur commande live box' });
+    		}
     	});
 	}	
 }
