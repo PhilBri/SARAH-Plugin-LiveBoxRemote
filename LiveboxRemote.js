4ae6f110ab6ce601514815f86e239f13dade8c2b
Phil Bri <ph.brioude@gmail.com>
2014-12-21 15:02:06 +0100

    V2.1 modified

--- LiveboxRemote.js
+++ LiveboxRemote.js
@@ -8,62 +8,60 @@
 |___________________________________________________|
 */
 
-var LiveBoxIP;
-
 exports.init = function ( SARAH ) {
 	var config = SARAH.ConfigManager.getConfig();
 
-	if ( /^autodetect$/i.test( config.modules.liveboxremote.livebox_IP ) == false ) {
-		return LiveBoxIP = config.modules.liveboxremote.livebox_IP;
-	}
+    if ( /^autodetect$/i.test( config.modules.liveboxremote.Livebox_IP ) == false ) return console.log('LiveBoxRemote => Autodetect [OFF]');
 
 	// Configure ip autodetection : (Auto Detect Plugin)
-	if ( ! SARAH.context.liveboxremote ) {
-		fsearch();
 
-		SARAH.listen ( 'autodetect', function ( data ) {
-			if ( data.from != 'LiveBoxRemote' ) fsearch();
-			else {
-				if ( LiveBoxIP ) console.log ( '\r\nLiveBoxRemote => Livebox : ip = ' + LiveBoxIP + ' (Auto Detect Plugin)');
-				else console.log ( '\r\nLiveBoxRemote => LiveBox : Non trouvée (Auto Detect Plugin)' );
-				SARAH.context.flag = false;
-			}
-		});
+    if ( !SARAH.context.liveboxremote ) {
+        fsearch();
+
+        SARAH.listen ( 'autodetect', function ( data ) {
+            if ( data.from != 'LiveBoxRemote' ) fsearch();
+            else {
+
+                if ( SARAH.context.liveboxremote.ip ) console.log ( '\r\nLiveboxRemote => Autodetect [ON] : ip = ' + SARAH.context.liveboxremote.ip );
+                else console.log ( '\r\nLiveboxRemote => Autodetect [ON] : ip non trouvée !' );
+                SARAH.context.flag = false;
+            }
+        });
 	}
 
 	function fsearch () {
-		if ( SARAH.context.flag != true ) {
-			SARAH.context.flag = true;
 
-			findLB = require( './lib/findLB.js' ) ( 'livebox', 'UHD', function ( RetIP ) {
-				SARAH.context.liveboxremote = { 'ip' : RetIP };
-				LiveBoxIP = SARAH.context.liveboxremote.ip;
-				SARAH.trigger ( 'autodetect', { 'from' : 'LiveBoxRemote' });
-			});
-		}
+        if ( SARAH.context.flag != true ) {
+            SARAH.context.flag = true;
+
+            findBR = require ( './lib/findLB.js' ) ( 'livebox', 'UHD', function ( RetIP ) {
+                SARAH.context.liveboxremote = { 'ip' : RetIP };
+                SARAH.trigger ( 'autodetect', { 'from' : 'LiveBoxRemote' });
+            });
+        }
 	}
 }
 
 exports.action = function ( data , callback , config , SARAH ) {
 
 	var	cmdArray = data.LBCode.split (','),
-		myReg = /\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b/;
-	
-	if ( ! myReg.test( SARAH.context..liveboxremote.ip ) && ! myReg.test( config.modules.liveboxremote.livebox_IP )) { 
-		return callback ({ 'tts' : 'Live box non trouvée' }) }
+		myReg = /\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b/,
+		LiveBoxIP;
 
-	LiveBoxIP = SARAH.context..liveboxremote.ip;
+    if ( typeof(SARAH.context.liveboxremote) != 'undefined' ) LiveBoxIP = SARAH.context.liveboxremote.ip
+    else if ( myReg.test( config.modules.liveboxremote.Livebox_IP ) == true ) LiveBoxIP = config.modules.liveboxremote.Livebox_IP
+    else return callback ({ 'tts' : 'Live box  non trouvée' })
 
 	sendLiveBox ( cmdArray );
 
 	function sendLiveBox ( cmdArray ) {
 
 		// Configure the request : NOTE -> The {qs} option add { ?operation=01&key=[LBCmd[123]]&mode=[LBCmd[5]] } at the end of {url}
-		var 	LBCmd 	= cmdArray.shift(),
-			request 	= require ( 'request' ),
+		var LBCmd 		= 	cmdArray.shift(),
+			request 	= 	require ( 'request' ),
 			options 	= 	{	url	: 'http://' + LiveBoxIP + ':8080/remoteControl/cmd',
-							qs	: { 'operation' : '01', 'key' : LBCmd.substr (0,3) , 'mode' : LBCmd.substr (4,1) }
-						}
+								qs	: { 'operation' : '01', 'key' : LBCmd.substr (0,3) , 'mode' : LBCmd.substr (4,1) }
+							}
 
 		request( options , function ( error , response , body ) {
 
